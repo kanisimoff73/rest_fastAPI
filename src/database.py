892d685d24a58449b3329970e7c6732749f266e1
kirill <kanisimoff73@gmail.com>
2023-10-09 14:27:48 +0700

    Change commit

--- src/database.py
+++ src/database.py
@@ -18,17 +18,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
-
-# DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-#
-# Base = declarative_base()
-#
-# async_engine = create_async_engine(DATABASE_URL)
-#
-# AsyncSessionLocal = sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
-#
-#
-# async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-#     async with AsyncSessionLocal() as session:
-#         yield session
