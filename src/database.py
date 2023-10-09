801b082c40dee4829d54dd04c6ddac9bc3a98b5e
kirill <kanisimoff73@gmail.com>
2023-10-09 17:16:35 +0700

    Change commit

--- src/database.py
+++ src/database.py
@@ -11,17 +11,6 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-check_sequence_query = text("SELECT 1 FROM pg_sequences WHERE schemaname = 'public' AND sequencename = 'task_sid_seq';")
-
-with engine.connect() as connection:
-    result = connection.execute(check_sequence_query)
-    sequence_exists = bool(result.fetchone())
-
-if not sequence_exists:
-    create_sequence_query = text("CREATE SEQUENCE task_sid_seq;")
-    with engine.connect() as connection:
-        connection.execute(create_sequence_query)
-
 
 def get_db():
     db = SessionLocal()
