3ad6eaed619f57985d1982c7e417d976c5d938a4
kirill <kanisimoff73@gmail.com>
2023-10-09 16:51:52 +0700

    Change commit

--- src/database.py
+++ src/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
@@ -10,6 +10,17 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+check_sequence_query = text("SELECT 1 FROM pg_sequences WHERE schemaname = 'public' AND sequencename = 'task_sid_seq';")
+
+with engine.connect() as connection:
+    result = connection.execute(check_sequence_query)
+    sequence_exists = bool(result.fetchone())
+
+if not sequence_exists:
+    create_sequence_query = text("CREATE SEQUENCE task_sid_seq;")
+    with engine.connect() as connection:
+        connection.execute(create_sequence_query)
 
 
 def get_db():
